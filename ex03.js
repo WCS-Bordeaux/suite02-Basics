c8d6e002ad9d7d62276429d725b8384bc357706b
Wiskyt <samy.bouamama@hotmail.fr>
2017-04-12 15:29:54 +0200

    5 premiers exercices

--- ex03.js
+++ ex03.js
@@ -13,3 +13,10 @@
         copyArray(["bonjour", 4, 5]); // ["bonjour", 4, 5]
 
 */
+
+function copyArray(arr) {
+   var newArr = [];
+   for (let i  = 0; i < arr.length; i++) {
+      newArr[i] = arr[i];
+   } return newArr;
+}
