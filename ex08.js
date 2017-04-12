352143b1eafd701ce778830fc0e67ca75a86ebbe
florian.lx <graphmatic.studio@gmail.com>
2017-04-12 20:09:25 +0200

    ex08.js done

--- ex08.js
+++ ex08.js
@@ -20,3 +20,30 @@
         // ["Oulala", "ça", "fait", "beaucoup", "de", "crochets!!!", "Pourtant c'est la même chose quasiment !"]
 
 */
+
+
+function getValuesFrom3DArray(arr) {
+
+    let newArr = [];
+    let i = 0;
+    let len = arr.length;
+    while( i < len ) {
+        insideLoop(arr[i]);
+        i++;
+    }
+
+    function insideLoop(key) {
+        if( Array.isArray(key)) {
+            let j = 0;
+            let lenIn = key.length;
+            while (j < lenIn) {
+                insideLoop(key[j]);
+                j++;
+            }
+        }
+        else {
+            newArr.push(key);
+        }
+    }
+    return newArr;
+}
