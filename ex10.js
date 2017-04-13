dab4b2e16fd92dbcf392b35721966a37631f0d62
florian.lx <graphmatic.studio@gmail.com>
2017-04-13 11:33:58 +0200

    ex10.js

--- ex10.js
+++ ex10.js
@@ -16,3 +16,33 @@
         Array.sort
 
 */
+
+function doubleSortNumbers(arr1, arr2) {
+
+    let i = 0;
+    let newArr = [];
+
+    for(v of arr1) {
+        newArr[v] = 0;
+    }
+    for (w of arr2) {
+        if(newArr[w] === 0) {
+            newArr[w] += 1;
+        }
+        else {
+            newArr[w] = 0;
+        }
+    }
+    for (k in newArr) {
+        arr1[i] = parseInt(k);
+        arr2[i] = parseInt(k);
+        i++;
+        let x = 0;
+        while( x < newArr[k] ) {
+            arr1[i] = parseInt(k);
+            arr2[i] = parseInt(k);
+            x++;
+            i++;
+        }
+    }
+}
