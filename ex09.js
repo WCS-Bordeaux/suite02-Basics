890c6072581e2b3f83a66c133d554253797e8d6c
florian.lx <graphmatic.studio@gmail.com>
2017-04-14 16:45:31 +0200

    ex09.js correction

--- ex09.js
+++ ex09.js
@@ -21,11 +21,21 @@ function sortNumbers(arr) {
     let i = 0;
     let newArr = [];
 
-    for(v of arr) {
-        newArr[v] = 0;
+    while (i < arr.length) {
+        if( newArr[arr[i]] >= 0 ) {
+            newArr.splice(arr[i]+1, 0, arr[i]);
+        }
+        else {
+            newArr[arr[i]]  = arr[i];
+        }
+        i++;
     }
-    for (k in newArr) {
-        arr[i] = parseInt(k);
+    i = 0;
+    arr.length =  0;
+    while( i < newArr.length ) {
+        if( newArr[i] !== undefined ) {
+            arr.push(newArr[i]);
+        }
         i++;
     }
 }
